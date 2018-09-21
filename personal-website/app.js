f2b89e7cdfc7024dceb0259e3b51153f46c68635
Maxim Chadliev <chad0441@gmail.com>
2018-09-21 15:42:40 -0500

    some animation

---
+++ personal-website/app.js
@@ -0,0 +1,11 @@
+function switchClass(i) {
+  var lis = $('#home-news > div');
+  lis.eq(i).removeClass('home_header_on');
+  lis.eq(i).removeClass('home_header_out');
+   lis.eq(i = ++i % lis.length).addClass('home_header_on');
+   lis.eq(i = ++i % lis.length).addClass('home_header_out');
+   setTimeout(function() {
+       switchClass(i);
+   }, 3500);
+}
+
