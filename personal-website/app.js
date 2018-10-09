a6728a41aba408230c731f9a9faa367ecc619757
Maxim Chadliev <chad0441@gmail.com>
2018-10-09 18:34:12 -0500

    blah

--- personal-website/app.js
+++ personal-website/app.js
@@ -1,178 +1,178 @@
-Letters = function() {
-    this.lettersDOM = null;
-    this.active = null;
-    this.letters = [];
-    this.alphabet = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","i","u","v","w","x","y","z","~","&","|","^","ç","@","]","[","{","}","ù","*","µ","¤","$","£","€","°",")","(","+","-","/","<",">","²","`","é","è","1","2","3","4","5","6","7","8","9","0"
-    ];
+// Letters = function() {
+//     this.lettersDOM = null;
+//     this.active = null;
+//     this.letters = [];
+//     this.alphabet = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","i","u","v","w","x","y","z","~","&","|","^","ç","@","]","[","{","}","ù","*","µ","¤","$","£","€","°",")","(","+","-","/","<",">","²","`","é","è","1","2","3","4","5","6","7","8","9","0"
+//     ];
     
   
-    return this;
-  };
-if (window.innerWidth > 760) {
+//     return this;
+//   };
+// if (window.innerWidth > 760) {
     
-    Letters.prototype.init = function (word) {
+//     Letters.prototype.init = function (word) {
     
-        this.lettersDOM = document.querySelectorAll('.letter');
-        this.active = true;
-        var i;
-        var nextChar;
-        var lettersMax = this.lettersDOM.length;
+//         this.lettersDOM = document.querySelectorAll('.letter');
+//         this.active = true;
+//         var i;
+//         var nextChar;
+//         var lettersMax = this.lettersDOM.length;
     
-        for (i = 0; i < this.lettersDOM.length; i++) {
+//         for (i = 0; i < this.lettersDOM.length; i++) {
       
-            if (word.charAt(i) != "")
-                nextChar = word.charAt(i);
-            else
-                nextChar = false;
+//             if (word.charAt(i) != "")
+//                 nextChar = word.charAt(i);
+//             else
+//                 nextChar = false;
       
-            this.letters.push(new Letter(this.lettersDOM[i], nextChar));
+//             this.letters.push(new Letter(this.lettersDOM[i], nextChar));
       
-        }
+//         }
     
-        if (word.length > lettersMax) {
+//         if (word.length > lettersMax) {
       
-            var wordContainer = document.getElementById("word");
+//             var wordContainer = document.getElementById("word");
   
-            for (i = lettersMax; i < word.length; i++) {
-                var letterSpan = document.createElement('span');
-                letterSpan.innerHTML = "";
-                letterSpan.classList.add('letter');
-                wordContainer.appendChild(letterSpan);
-                this.letters.push(new Letter(letterSpan, word.charAt(i)));
-            }
-        }
+//             for (i = lettersMax; i < word.length; i++) {
+//                 var letterSpan = document.createElement('span');
+//                 letterSpan.innerHTML = "";
+//                 letterSpan.classList.add('letter');
+//                 wordContainer.appendChild(letterSpan);
+//                 this.letters.push(new Letter(letterSpan, word.charAt(i)));
+//             }
+//         }
     
-        this.animate();
+//         this.animate();
     
-        return this;
+//         return this;
     
-    };
+//     };
   
-    Letters.prototype.animate = function () {
-        var i;
-        var random;
-        var char;
+//     Letters.prototype.animate = function () {
+//         var i;
+//         var random;
+//         var char;
     
-        if (this.active) {
+//         if (this.active) {
    
-            window.requestAnimationFrame(this.animate.bind(this));
+//             window.requestAnimationFrame(this.animate.bind(this));
       
-            var indexes = [];
+//             var indexes = [];
   
-            for (i = 0; i < this.letters.length; i++) {
+//             for (i = 0; i < this.letters.length; i++) {
       
-                var current = this.letters[i];
+//                 var current = this.letters[i];
         
-                if (!current.isDead) {
-                    random = Math.floor(Math.random() * (this.alphabet.length - 0));
-                    char = this.alphabet[random];
-                    current.render(char);
-                } else {
-                    indexes.push(i);
-                }
-            }
+//                 if (!current.isDead) {
+//                     random = Math.floor(Math.random() * (this.alphabet.length - 0));
+//                     char = this.alphabet[random];
+//                     current.render(char);
+//                 } else {
+//                     indexes.push(i);
+//                 }
+//             }
       
-            for (i = 0; i < indexes.length; i++) {
-                this.letters.splice(indexes[i], 1);
-            }
+//             for (i = 0; i < indexes.length; i++) {
+//                 this.letters.splice(indexes[i], 1);
+//             }
       
-            if (this.letters.length == 0) {
-                this.stop();
-            }
-        }
-    };
+//             if (this.letters.length == 0) {
+//                 this.stop();
+//             }
+//         }
+//     };
   
-    Letters.prototype.start = function (word) {
-        this.init(word);
-    };
+//     Letters.prototype.start = function (word) {
+//         this.init(word);
+//     };
   
-    Letters.prototype.stop = function () {
-        this.active = false;
-    };
+//     Letters.prototype.stop = function () {
+//         this.active = false;
+//     };
   
-    Letter = function (DOMElement, nextChar) {
+//     Letter = function (DOMElement, nextChar) {
     
-        var scope = this;
+//         var scope = this;
     
-        this.DOMEl = DOMElement;
-        this.char = DOMElement.innerHTML;
-        this.next = nextChar;
-        this.speed = Math.floor(Math.random() * (500 - 10));
-        this.total = 0;
-        this.duration = 2000;
-        this.animating = true;
-        this.isDead = false;
+//         this.DOMEl = DOMElement;
+//         this.char = DOMElement.innerHTML;
+//         this.next = nextChar;
+//         this.speed = Math.floor(Math.random() * (500 - 10));
+//         this.total = 0;
+//         this.duration = 2000;
+//         this.animating = true;
+//         this.isDead = false;
     
-        this.timer = setInterval(function () {
-            if (scope.animating === true) {
-                scope.total += scope.speed;
-            }
-            scope.animating = !scope.animating;
-        }, this.speed);
+//         this.timer = setInterval(function () {
+//             if (scope.animating === true) {
+//                 scope.total += scope.speed;
+//             }
+//             scope.animating = !scope.animating;
+//         }, this.speed);
   
-        this.animate();
+//         this.animate();
     
-        return this;
+//         return this;
    
-    };
+//     };
   
-    Letter.prototype.animate = function () {
-        var i;
-        var random;
+//     Letter.prototype.animate = function () {
+//         var i;
+//         var random;
     
-        if (!this.isDead) {
-            window.requestAnimationFrame(this.animate.bind(this));
-        }
+//         if (!this.isDead) {
+//             window.requestAnimationFrame(this.animate.bind(this));
+//         }
     
     
-        if (this.total < this.duration) {
+//         if (this.total < this.duration) {
       
-            if (this.animating) {
-                this.DOMEl.innerHTML = this.char;
-            }
+//             if (this.animating) {
+//                 this.DOMEl.innerHTML = this.char;
+//             }
         
-        } else {
-            this.isDead = true;
+//         } else {
+//             this.isDead = true;
       
-            if (!this.next) {
-                var parent = document.getElementById('word');
-                parent.removeChild(this.DOMEl);
-                return;
-            }
+//             if (!this.next) {
+//                 var parent = document.getElementById('word');
+//                 parent.removeChild(this.DOMEl);
+//                 return;
+//             }
       
-            this.DOMEl.innerHTML = this.next;
-        }
-    };
+//             this.DOMEl.innerHTML = this.next;
+//         }
+//     };
   
-    Letter.prototype.render = function (char) {
+//     Letter.prototype.render = function (char) {
     
-        if (!this.animating) {
-            this.char = char;
-        }
+//         if (!this.animating) {
+//             this.char = char;
+//         }
     
-    };
+//     };
   
-    var word = ["FULL-STACK    DEVELOPER", "MAX    CHADLIEV"];
-    var nextWord = 1;
+//     var word = ["DEVELOPER","BACK-END","FRONT-END", "FULL-STACK"];
+//     var nextWord = 1;
   
-    var letters = new Letters();
+//     var letters = new Letters();
   
-    setTimeout(function () {
+//     setTimeout(function () {
     
-        letters.start(word[nextWord]);
+//         letters.start(word[nextWord]);
     
-        setInterval(function () {
-            nextWord++;
-            if (nextWord >= word.length)
-                nextWord = 0;
+//         setInterval(function () {
+//             nextWord++;
+//             if (nextWord >= word.length)
+//                 nextWord = 0;
       
-            letters.start(word[nextWord]);
-        }, 10000);
+//             letters.start(word[nextWord]);
+//         }, 8000);
     
-    }, 4000);
+//     }, 4000);
   
   
-}
+// }
 
 
 
@@ -190,4 +190,10 @@ $(document).ready(function() {
     }).mouseleave(function() {
       $(this).removeClass(classClicked);
     });
-  })
+})
+
+
+
+
+  
+
