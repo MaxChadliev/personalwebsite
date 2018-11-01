d8ce693c9997eba52c1c0d4b277ad0f7f70e570b
Maxim Chadliev <chad0441@gmail.com>
2018-10-31 20:29:09 -0500

    blah

--- personal-website/app.js
+++ personal-website/app.js
@@ -10,20 +10,21 @@ $(document).ready(function() {
 
 
 
-$(document).ready(function() {
-    var classClicked = 'clicked';
-    $('.card-flip').click(function() {
-      if($(this).hasClass(classClicked)) {
-        $(this).removeClass(classClicked);
-      }
-      else {
-        $(this).addClass(classClicked);
-      }
-    }).mouseleave(function() {
+$(document).ready(function () {
+  var classClicked = 'clicked';
+  $('.card-flip').click(function () {
+    if ($(this).hasClass(classClicked)) {
       $(this).removeClass(classClicked);
-    });
+    }
+    else {
+      $(this).addClass(classClicked);
+    }
+  }).mouseleave(function () {
+    $(this).removeClass(classClicked);
+  });
 })
 
+  ;
 
 
 
